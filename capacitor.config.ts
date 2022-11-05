6cbc6477baf11222d47dd8a044adcbb0343bf358
Dantedbk <101567058+Dantedbk@users.noreply.github.com>
2022-11-05 17:52:22 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proiectowe',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
